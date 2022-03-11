ee5f134b0d3bfa0fe1d00446d8ad2de65121e8b2
apazze <apazze@users.noreply.github.com>
2022-03-11 11:53:30 -0300

    Criado teste para Recuperar lugares com filtro de cidade e datas.

--- DB.AirBDB.DAL.RepositoryTests/LugarDAOTests.cs
+++ DB.AirBDB.DAL.RepositoryTests/LugarDAOTests.cs
@@ -7,6 +7,7 @@ using FluentAssertions;
 using FluentAssertions.Execution;
 using Microsoft.EntityFrameworkCore;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -16,7 +17,7 @@ namespace DB.AirBDB.DAL.RepositoryTests
     public class LugarDAOTests
     {
         [Fact]
-        public void RecuperaLugares_FiltroPorCidade()
+        public void RecuperaLugares_FiltroPorCidade_RetornaRegistros()
         {
             //Assert
             var lista = new ListasFake();
@@ -39,6 +40,99 @@ namespace DB.AirBDB.DAL.RepositoryTests
             {
                 resultado.Should().NotBeNull();
                 resultado.Count().Should().Be(2);
+            }
+        }
+
+        [Fact]
+        public void RecuperaLugares_FiltroPorCidade_RetornaListaVazia()
+        {
+            //Assert
+            var lista = new ListasFake();
+
+            var contextMock = new Mock<AppDBContext>();
+            contextMock.Object.Lugares = CreateDbSetMock(lista.Lugares).Object;
+
+            var configLugar = new MapperConfiguration(cfg => cfg.CreateMap<Lugar, LugarDTO>().ReverseMap());
+            var mapperLugar = configLugar.CreateMapper();
+
+            ILugarDAO sut = new LugarDAO(contextMock.Object, mapperLugar);
+
+            LugarFiltroDTO filtro = new LugarFiltroDTO() { Cidade = "Porto Alegres" };
+
+            //Act
+            var resultado = sut.Filtrar(filtro);
+
+            //Assert
+            using (new AssertionScope())
+            {
+                resultado.Should().BeEmpty();
+            }
+        }
+
+        [Fact]
+        public void RecuperaLugares_FiltroPorCidadeEDatasDisponiveis_RetornaRegistros()
+        {
+            //Assert
+            var lista = new ListasFake();
+
+            var contextMock = new Mock<AppDBContext>();
+            contextMock.Object.Lugares = CreateDbSetMock(lista.Lugares).Object;
+            contextMock.Object.Reservas = CreateDbSetMock(lista.Reservas).Object;
+            contextMock.Object.Usuarios = CreateDbSetMock(lista.Usuarios).Object;
+
+            var configLugar = new MapperConfiguration(cfg => cfg.CreateMap<Lugar, LugarDTO>().ReverseMap());
+            var mapperLugar = configLugar.CreateMapper();
+
+            ILugarDAO sut = new LugarDAO(contextMock.Object, mapperLugar);
+
+            LugarFiltroDTO filtro = new LugarFiltroDTO()
+            {
+                Cidade = "Porto Alegre",
+                DataInicio = new DateTime(2022, 6, 1, 8, 0, 0).ToString(),
+                DataFim = new DateTime(2022, 6, 20, 18, 0, 0).ToString(),
+            };
+
+            //Act
+            var resultado = sut.Filtrar(filtro);
+
+            //Assert
+            using (new AssertionScope())
+            {
+                resultado.Should().NotBeNull();
+                resultado.Count().Should().Be(2);
+            }
+        }
+
+        [Fact]
+        public void RecuperaLugares_FiltroPorCidadeEDatasOcupadas_RetornaVazio()
+        {
+            //Assert
+            var lista = new ListasFake();
+
+            var contextMock = new Mock<AppDBContext>();
+            contextMock.Object.Lugares = CreateDbSetMock(lista.Lugares).Object;
+            contextMock.Object.Reservas = CreateDbSetMock(lista.Reservas).Object;
+            contextMock.Object.Usuarios = CreateDbSetMock(lista.Usuarios).Object;
+
+            var configLugar = new MapperConfiguration(cfg => cfg.CreateMap<Lugar, LugarDTO>().ReverseMap());
+            var mapperLugar = configLugar.CreateMapper();
+
+            ILugarDAO sut = new LugarDAO(contextMock.Object, mapperLugar);
+
+            LugarFiltroDTO filtro = new LugarFiltroDTO()
+            {
+                Cidade = "Gravataí",
+                DataInicio = new DateTime(2022, 9, 1, 8, 0, 0).ToString(),
+                DataFim = new DateTime(2022, 9, 20, 18, 0, 0).ToString(),
+            };
+
+            //Act
+            var resultado = sut.Filtrar(filtro);
+
+            //Assert
+            using (new AssertionScope())
+            {
+                resultado.Should().BeEmpty();
             }
         }
 
